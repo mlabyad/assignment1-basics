d2a9ffea5c5d9bb6f7a3af842d9602a03123af72
D641771 <marouane.labyad@safrangroup.com>
2026-07-11 20:12:00 +0200

    wip

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -38,18 +38,56 @@ def train_bpe(
                 Merges are ordered by order of creation.
     """
 
-    corpus = read_corpus(input_path, pct=1)
-    pre_tokenized_corpus = pre_tokenize(corpus, special_tokens)
+    corpus_chuncks = read_corpus(input_path, nbr_chunks=4, pct=1, special_tokens=special_tokens)
+    pre_tokenized_corpus = pre_tokenize(corpus_chuncks, special_tokens)
     vocab, merges = learn_bpe(pre_tokenized_corpus, vocab_size, special_tokens)
     return vocab, merges
 
-def read_corpus(input_path: str | os.PathLike, pct: float = 1) -> list[str]:
+def read_corpus(input_path: str, nbr_chunks: int = 3, pct: float = 1, special_tokens: list[bytes] = None) -> list[int]:
     """Read the corpus from the given path and return a list of strings.
 
     Args:
-        input_path (str | os.PathLike): Path to BPE tokenizer training data.
+        input_path (str): Path to BPE tokenizer training data.
         pct (float, optional): Percentage of the corpus to read. Defaults to 1 (read all).
 
     Returns:
         list[str]: List of strings in the corpus.
     """
+    file = open(input_path, "rb")
+    file.seek(0, os.SEEK_END)
+    total_size = file.tell()
+    file.seek(0)
+
+    last_byte = int(total_size * pct)
+    chunk_size = int(last_byte // nbr_chunks)
+
+    chunks = [i * chunk_size for i in range(nbr_chunks+1)]
+    chunks[-1] = last_byte
+    print(total_size)
+    print(last_byte)
+    print(chunks)
+
+    mini_chunk_size = 4028
+    for i in range(1, len(chunks)-1):
+        curr_position = chunks[i]
+        file.seek(chunks[i])
+        while True:
+            chunk_content = file.read(mini_chunk_size)
+
+            best_pos = -1
+            for token in special_tokens:
+                pos = chunk_content.find(token)
+                if pos == -1 and (best_pos == -1 or pos < best_pos):
+                    best_pos = pos
+
+            if best_pos != -1:
+                chunks[i] = best_pos + curr_position
+                break
+            curr_position += mini_chunk_size
+    return sorted(set(chunks))
+
+
+if __name__ == "__main__":
+    special_tokens = ['<|endoftext|>']
+    encoded_special_tokens = [i.encode('UTF-8') for i in special_tokens]
+    read_corpus("data/TinyStoriesV2-GPT4-train.txt", 3, 0.2, encoded_special_tokens)
